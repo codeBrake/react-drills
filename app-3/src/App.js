2bd7b5819108a251c137e006ca6e9ab6a7672e32
Austin <brake_austin@yahoo.com>
2018-10-22 15:40:33 -0600

    Finished3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      userInput: '',
+      array: ['Tony', 'Will', 'Jack']
+    }
+  }
+ 
+  filterArray(val){
+    this.setState({userInput: val})
+  }
+  
+  
   render() {
+    let array = this.state.array.filter((element)=>{
+      return element.startsWith(this.state.userInput);
+    }).map((arr,i)=>{
+      return <h1 key={i}>{arr}</h1>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e) => this.filterArray(e.target.value)}></input>
+        {array}
       </div>
     );
   }
